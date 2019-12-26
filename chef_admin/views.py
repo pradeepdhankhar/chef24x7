eecf988374c33a295a3316bc65a5e10acf06272d
Shorya Vaish <shorya.vaish2020@gmail.com>
2019-12-26 17:45:46 +0530

    added new urls

--- chef_admin/views.py
+++ chef_admin/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hellow World  you are at the sample")
+	return HttpResponse("Hello World  you are at the sample")
